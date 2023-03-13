54d387c6583671052c1daa6a2c4f4c33e971a359
Sudeep Paul <sudeeprnp@gmail.com>
2023-03-13 20:24:18 +0530

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import styles from "@/styles/Home.module.css";
+import NavBar from "../components/NavBar";
+
+
+function Home() {
+  return (
+    <>
+      <NavBar />
+  
+      <main className={styles.main}>
+        <h1 className={styles.h1}>
+          Welcome to our Decentralized Lending Platform
+        </h1>
+        <p className={styles.p}>
+          MUX-YOUR-XRD, a lending platform allows you to borrow and lend money without the
+          need for a centralized intermediary. By using smart contracts on a
+          blockchain network, we enable peer-to-peer lending with transparent
+          and secure transactions.
+
+          We make P2P Lending more risk resistant and more fun !
+        </p>
+        <p>
+          Whether you're looking to borrow money at a lower interest rate or
+          lend money to earn higher returns, our platform provides a
+          decentralized alternative to traditional lending.
+        </p>
+
+      </main>
+    </>
+  );
+}
+
+export default Home;
