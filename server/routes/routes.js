4650237588299b41dd63b9ffd6c534b5cea70f0d
Sudeep Paul <sudeeprnp@gmail.com>
2023-03-19 01:00:58 +0530

    Database working

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -8,7 +8,7 @@ const User = require("../models/user");
 
 // API Endpoint for posting a loan request, which also updates Borrower model
 // >> {account_address, currency_code, loan_amount, loan_duration, interest_rate, funders(emptyarray), collateral_currency_code }
-// A) Tested >> Working 
+// A) Tested >> Working
 
 router.post("/borrow", async (req, res) => {
   try {
@@ -17,108 +17,113 @@ router.post("/borrow", async (req, res) => {
       account_address: req.body.account_address,
     });
 
-    query1.then((user) => {
-      if (!user) {
-        res.status(400).json({ message: "User not found" });
-      } else {
-        // Update the collateral array of the user
-        let user_collateral = user.collateral;
-        let amount = 0;
+    query1
+      .then((user) => {
+        if (!user) {
+          res.status(400).json({ message: "User not found" });
+        } else {
+          // Update the collateral array of the user
+          let user_collateral = user.collateral;
+          let amount = 0;
 
-        for (let i = 0; i < user_collateral.length; i++) {
-          if (
-            user_collateral[i].currency_code ===
-            req.body.collateral_currency_code
-          ) {
-            amount += user_collateral[i].collateral_amount;
-            user_collateral[i].collateral_amount = user_collateral[i].collateral_amount - req.body.loan_amount;
-            break;
+          for (let i = 0; i < user_collateral.length; i++) {
+            if (
+              user_collateral[i].currency_code ===
+              req.body.collateral_currency_code
+            ) {
+              amount += user_collateral[i].collateral_amount;
+              user_collateral[i].collateral_amount =
+                user_collateral[i].collateral_amount - req.body.loan_amount;
+              break;
+            }
+          }
+
+          if (amount < req.body.loan_amount) {
+            res.status(400).json({ message: "Insufficient Collateral" });
+          } else {
+            // save the updated user collateral to the database
+            User.findOneAndUpdate(
+              { account_address: req.body.account_address },
+              { $set: { collateral: user_collateral } },
+              { new: true }
+            ).then(() => {
+              const loan = new Loan({
+                account_address: req.body.account_address,
+                currency_code: req.body.currency_code,
+                loan_amount: req.body.loan_amount,
+                loan_duration: req.body.loan_duration,
+                interest_rate: req.body.interest_rate,
+                funders: req.body.funders,
+                collateral_currency_code: req.body.collateral_currency_code,
+              });
+              loan.save().then(() => {
+                // 2)  Updating Borrower Model
+                const query2 = Borrower.findOne({
+                  account_address: req.body.account_address,
+                });
+
+                query2
+                  .then(async (borrower) => {
+                    if (!borrower) {
+                      // the borrower has placed a loan request for the first time
+                      const new_requested_loans = [loan._id];
+
+                      // creating the object
+                      const new_borrower = Borrower({
+                        account_address: req.body.account_address,
+                        requested_loans: new_requested_loans,
+                      });
+                      await new_borrower.save();
+                    } else {
+                      // the borrower has already placed one or more loan requests
+                      let new_requested_loans = borrower.requested_loans;
+                      new_requested_loans.push(loan._id);
+
+                      // updating the object
+                      await Borrower.updateOne(
+                        { account_address: req.body.account_address },
+                        { $set: { requested_loans: new_requested_loans } }
+                      );
+                    }
+
+                    // Send the response after all asynchronous operations have completed
+                    res.status(201).json(loan);
+                  })
+                  .catch((err) => {
+                    res.status(400).json({ message: err.message });
+                  });
+              });
+            });
           }
         }
-
-        if (amount < req.body.loan_amount) {
-          res.status(400).json({ message: "Insufficient Collateral" });
-        } else {
-          // save the updated user collateral to the database
-          User.findOneAndUpdate(
-            { account_address: req.body.account_address },
-            {$set: { collateral: user_collateral }},
-            { new: true }
-          ).then(() => {
-            const loan = new Loan({
-              account_address: req.body.account_address,
-              currency_code: req.body.currency_code,
-              loan_amount: req.body.loan_amount,
-              loan_duration: req.body.loan_duration,
-              interest_rate: req.body.interest_rate,
-              funders: req.body.funders,
-              collateral_currency_code: req.body.collateral_currency_code,
-            });
-            loan.save().then(() => {
-              // 2)  Updating Borrower Model
-              const query2 = Borrower.findOne({
-                account_address: req.body.account_address,
-              });
-
-              query2
-                .then(async (borrower) => {
-                  if (!borrower) {
-                    // the borrower has placed a loan request for the first time
-                    const new_requested_loans = [loan._id];
-
-                    // creating the object
-                    const new_borrower = Borrower({
-                      account_address: req.body.account_address,
-                      requested_loans: new_requested_loans,
-                    });
-                    await new_borrower.save();
-                  } else {
-                    // the borrower has already placed one or more loan requests
-                    let new_requested_loans = borrower.requested_loans;
-                    new_requested_loans.push(loan._id);
-
-                    // updating the object
-                    await Borrower.updateOne(
-                      { account_address: req.body.account_address },
-                      { $set: { requested_loans: new_requested_loans } }
-                    );
-                  }
-
-                  // Send the response after all asynchronous operations have completed
-                  res.status(201).json(loan);
-                })
-                .catch((err) => {
-                  res.status(400).json({ message: err.message });
-                });
-            });
-          });
-        }
-      }
-    }).catch((err) => {
-      res.status(400).json({ message: err.message });
-    });
+      })
+      .catch((err) => {
+        res.status(400).json({ message: err.message });
+      });
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
 });
 
-
 // API Endpoint for posting a lend request which also changes the Loan Model
 // >> (account_address, loan_id, funding_amount)
-// B)
+// B) tested >> Working
 
 router.post("/lend", async (req, res) => {
-  try {
+  try{  // ends at 209
     // 3) Updating the Loan Model
     const query3 = Loan.findOne({
-      loan_id: req.body.loan_id,
+      _id: req.body.loan_id,
     });
 
-    query3
-      .then(async (loan) => {
+    query3 // 119- 208
+      .then((loan) => {
         if (!loan) {
           res.status(400).json({ message: "Loan Id is not valid" });
-        } else {
+        } else {  // ends at 207
+          let new_loan_amount = loan.loan_amount;
+          new_loan_amount -= req.body.funding_amount;
+          
           let new_funders = loan.funders;
           let present = false;
           let index_present = -1;
@@ -129,8 +134,9 @@ router.post("/lend", async (req, res) => {
               break;
             }
           }
+          
           if (present) {
-            new_funders[i].funding_amount += req.body.funded_amount;
+            new_funders[index_present].funding_amount += req.body.funding_amount;
           } else {
             new_funders.push({
               funder_address: req.body.account_address,
@@ -138,76 +144,74 @@ router.post("/lend", async (req, res) => {
             });
           }
 
-          // updating the object
-          await Loan.updateOne(
-            { loan_id: req.body.loan_id },
-            { $set: { funders: new_funders } }
-          );
-        }
-      })
-      .catch((err) => {
-        res.status(400).json({ message: err.message });
-      });
-
-    // 4) Updating Lender Model
-    const query4 = Lender.findOne({
-      account_address: req.body.account_address,
-    });
-
-    query4
-      .then(async (lender) => {
-        if (!lender) {
-          // the lender has placed a lend request for the first time
-          const new_funded_loans = [
-            {
-              loan_id: req.body.loan_id,
-              funding_amount: req.body.funding_amount,
-            },
-          ];
-
-          // creating the object
-          const new_lender = Lender({
-            account_address: req.body.account_address,
-            funded_loans: new_funded_loans,
-          });
-          await new_lender.save();
-        } else {
-          // the lender has already placed one or more lend requests
-          let new_funded_loans = lender.funded_loans;
-          let present = false;
-          let index_present = -1;
-          for (let i = 0; i < new_funded_loans.length; i++) {
-            if (new_funded_loans[i].loan_id === req.body.loan_id) {
-              present = true;
-              index_present = i;
-              break;
-            }
-          }
-          if (present) {
-            new_funded_loans[i].funding_amount += req.body.funded_amount;
-          } else {
-            new_funded_loans.push({
-              loan_id: req.body.loan_id,
-              funding_amount: req.body.funding_amount,
+          // updating the object 147- 206 
+          Loan.updateOne(
+            { _id: req.body.loan_id },
+            { $set: { funders: new_funders, loan_amount: new_loan_amount } }
+          ).then(async () => {
+            // 4) Updating Lender Model
+            const query4 = Lender.findOne({
+              account_address: req.body.account_address,
             });
-          }
-
-          // updating the object
-          await Lender.updateOne(
-            { account_address: req.body.account_address },
-            { $set: { funded_loans: new_funded_loans } }
-          );
-        }
-      })
-      .catch((err) => {
-        res.status(400).json({ message: err.message });
-      });
-
-    res.status(201).json({ message: "success" });
-  } catch (err) {
+    
+            query4  // 157 -205
+              .then(async (lender) => {
+                if (!lender) {
+                  // the lender has placed a lend request for the first time
+                  const new_funded_loans = [
+                    {
+                      loan_id: req.body.loan_id,
+                      funding_amount: req.body.funding_amount,
+                    },
+                  ];
+    
+                  // creating the object
+                  const new_lender = new Lender({
+                    account_address: req.body.account_address,
+                    funded_loans: new_funded_loans,
+                  });
+                  await new_lender.save();
+                } else {
+                  // the lender has already placed one or more lend requests
+                  let new_funded_loans = lender.funded_loans;
+                  let present = false;
+                  let index_present = -1;
+                  for (let i = 0; i < new_funded_loans.length; i++) {
+                    if (new_funded_loans[i].loan_id === req.body.loan_id) {
+                      present = true;
+                      index_present = i;
+                      break;
+                    }
+                  }
+                  if (present) {
+                    new_funded_loans[index_present].funding_amount += req.body.funding_amount;
+                  } else {
+                    new_funded_loans.push({
+                      loan_id: req.body.loan_id,
+                      funding_amount: req.body.funding_amount,
+                    });
+                  }
+    
+                  // updating the object
+                  await Lender.updateOne(
+                    { account_address: req.body.account_address },
+                    { $set: { funded_loans: new_funded_loans } }
+                  );
+                }
+                res.status(201).json({ message: "success" });
+              })
+              .catch((err) => {
+                res.status(400).json({ message: err.message });
+              });
+        })
+  } 
+  })
+}
+  catch (err) {
     res.status(400).json({ message: err.message });
   }
 });
+
 
 // API Endpoint for signing in and creating an object in the user Model
 // >> {account_address, name, password, collateral(empty array)}
